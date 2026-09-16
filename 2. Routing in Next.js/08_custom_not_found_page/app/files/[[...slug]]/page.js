b41570820ea5f31c0d60bd8c55fabbb34c8a3dbe
Prabhat <osrprabhatyadav4@gmail.com>
2026-09-16 11:54:49 +0530

    added custom not found page

---
+++ 2. Routing in Next.js/08_custom_not_found_page/app/files/[[...slug]]/page.js
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: {
+    absolute: "My Files",
+  },
+};
+
+export default async function DocsPage({ params }) {
+  const { slug = [] } = await params;
+
+  return (
+    <main>
+      <h1>Documentation</h1>
+      <p>{JSON.stringify(slug)}</p>
+    </main>
+  );
+}
---
+++ 2. Routing in Next.js/08_custom_not_found_page/app/layout.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: {
+    template: "%s | KernalPrab",
+    default: "KernalPrab"
+  }
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <nav style={{ display: "flex", gap: "1rem" }}>
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/services">Services</Link>
+          </nav>
+        </header>
+
+        <main>{children}</main>
+
+        <footer>
+          <p>&copy; 2026 Demo App. All rights reserved.</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
